894df1acadb4698c86645b0120237722d76b9c71
Andre Keizer <andre.keizer@itspeople.nl>
2026-04-30 17:29:47 +0200

    config: remove basePath, add CNAME for www.itspeople.nl

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const basePath = "/itspeople-website";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath,
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
